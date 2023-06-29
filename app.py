bcef559e0c6be15297a34622f5bf87d8c3818152
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-29 16:55:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,51 +1,43 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        l1 = int(request.form['l1'])
+        l2 = int(request.form['l2'])
+        iv = request.form['iv']
+        return render_template('password.html', l1=l1, l2=l2, iv=iv)
     return render_template('index.html')
 
-@app.route('/check-password', methods=['POST'])
-def check_password():
-    # Get the form data from the request
-    data = request.get_json()
-
-    # Extract the password and validation criteria
-    password = data['password']
-    min_length = int(data['minLength'])
-    max_length = int(data['maxLength'])
-    invalid_chars = data['invalidChars']
-
-    # Perform the password validity check
-    if not is_valid_password(password, min_length, max_length, invalid_chars):
-        result = {'valid': False, 'reason': 'Password is not valid.'}
-    else:
-        result = {'valid': True}
-
-    # Render the result.html template with the result data
+@app.route('/validate', methods=['POST'])
+def validate():
+    password = request.form['password']
+    l1 = int(request.form['l1'])
+    l2 = int(request.form['l2'])
+    iv = request.form['iv']
+    result = validate_password(password, l1, l2, iv)
     return render_template('results.html', result=result)
 
-def is_valid_password(password, min_length, max_length, invalid_chars):
-    # Check password length
-    if len(password) < min_length or len(password) > max_length:
-        return False
-
-    # Check for at least one number
-    if not any(char.isdigit() for char in password):
-        return False
-
-    # Check for at least two uppercase letters
-    if sum(char.isupper() for char in password) < 2:
-        return False
-
-    # Check for invalid characters
-    if any(char in password for char in invalid_chars):
-        return False
-
-    # Additional checks if needed...
-
-    return True
+def validate_password(password, l1, l2, iv):
+    # Your password validation logic here
+    # Return a dictionary with 'valid' and 'reason' keys
+    
+    # Example implementation
+    # You can replace this with your actual password validation logic
+    if len(password) < l1 or len(password) > l2:
+        return {'valid': False, 'reason': f"Password length should be between {l1} and {l2} characters."}
+    elif not any(char.isdigit() for char in password):
+        return {'valid': False, 'reason': 'Password must contain at least one digit.'}
+    elif len([char for char in password if char.isupper()]) < 2:
+        return {'valid': False, 'reason': 'Password must contain at least two uppercase letters.'}
+    elif not any(char in '#@+-%' for char in password):
+        return {'valid': False, 'reason': 'Password must contain at least one of the characters: #@+-%.'}
+    elif any(char in iv for char in password):
+        return {'valid': False, 'reason': 'Password cannot contain the characters from the invalid list.'}
+    else:
+        return {'valid': True, 'reason': None}
 
 if __name__ == '__main__':
     app.run()
