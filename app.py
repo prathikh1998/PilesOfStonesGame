e9167285ea5ab5ecb07db687d1a94b42dba8492f
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-30 01:13:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,55 +1,86 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from multiprocessing import Process, Manager
+from redis import Redis
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+socketio = SocketIO(app)
+redis = Redis()
 
-@app.route('/', methods=['GET', 'POST'])
+class Game:
+    def __init__(self, pile1, pile2, pile3, min_pickup, max_pickup, player1, player2):
+        self.piles = [pile1, pile2, pile3]
+        self.min_pickup = min_pickup
+        self.max_pickup = max_pickup
+        self.players = [player1, player2]
+        self.current_player = 0
+        self.scores = [0, 0]
+
+    def take_turn(self, pile_index, stones_taken):
+        pile = self.piles[pile_index]
+        if stones_taken < self.min_pickup or stones_taken > self.max_pickup or pile < stones_taken:
+            return False
+
+        pile -= stones_taken
+        self.piles[pile_index] = pile
+        self.scores[self.current_player] += stones_taken
+
+        self.current_player = (self.current_player + 1) % 2
+        return True
+
+    def is_game_over(self):
+        return all(pile == 0 for pile in self.piles)
+
+    def get_winner(self):
+        if self.scores[0] > self.scores[1]:
+            return self.players[0]
+        elif self.scores[1] > self.scores[0]:
+            return self.players[1]
+        else:
+            return "It's a tie!"
+
+games = {}
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        text_input = request.form['text_input']
-        word = request.form['word']
-        words = request.form['words']
-        modified_text = modify_text(text_input, word, words)
-        return render_template('results.html', modified_text=modified_text)
     return render_template('index.html')
 
-def modify_text(text_input, word, words):
-    modified_text = text_input
+@app.route('/start', methods=['POST'])
+def start_game():
+    pile1 = int(request.form['pile1'])
+    pile2 = int(request.form['pile2'])
+    pile3 = int(request.form['pile3'])
+    min_pickup = int(request.form['min_pickup'])
+    max_pickup = int(request.form['max_pickup'])
+    player1 = request.form['player1']
+    player2 = request.form['player2']
 
-    if word:
-        modified_text = remove_word(modified_text, word)
+    game = Game(pile1, pile2, pile3, min_pickup, max_pickup, player1, player2)
+    game_id = len(games) + 1
+    games[game_id] = game
 
-    if words:
-        modified_text = find_words(modified_text, words)
+    socketio.emit('new_game', {'game_id': game_id, 'game': game}, broadcast=True)
 
-    return modified_text
+    return ''
 
-def remove_word(text, word):
-    return text.replace(word, '')
+@app.route('/play', methods=['POST'])
+def play():
+    game_id = int(request.form['game_id'])
+    pile_index = int(request.form['pile_index'])
+    stones_taken = int(request.form['stones_taken'])
 
-def find_words(text, words):
-    w1, w2 = words.split()
+    game = games[game_id]
+    success = game.take_turn(pile_index, stones_taken)
 
-    occurrences = []
-    index = 0
+    if game.is_game_over():
+        winner = game.get_winner()
+        del games[game_id]
+        socketio.emit('game_over', {'winner': winner}, broadcast=True)
+    else:
+        socketio.emit('game_update', {'game_id': game_id, 'game': game, 'success': success}, broadcast=True)
 
-    while index < len(text):
-        found_index = text.find(w1, index)
-        if found_index == -1:
-            break
-
-        next_index = found_index + len(w1)
-        if next_index < len(text) and text[next_index] == ' ':
-            next_index += 1
-
-        if text[next_index:].startswith(w2):
-            occurrences.append((found_index, found_index + len(w1) + len(w2)))
-
-        index = next_index
-
-    for start, end in reversed(occurrences):
-        text = text[:start] + text[end:]
-
-    return text
+    return ''
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, host='0.0.0.0', port=5000)
