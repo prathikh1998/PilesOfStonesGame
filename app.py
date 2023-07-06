ad7792b3ea0169465dc5dded9d588fb8b0226aaf
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-07-06 15:54:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,74 +1,84 @@
+import time
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import redis
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
 
-# Initialize game state
-redis_host = 'ass6cache.redis.cache.windows.net'
-redis_port = 6379
-redis_password = 'ugMdQa89swtNhUNPnXsoXNwqzjv4WWDYqAzCaPwhKNA='
-GAME_STATE_KEY = "game_state"
-DEFAULT_PILES = [5, 4, 3]
-DEFAULT_PLAYERS = ['Player 1', 'Player 2']
+class Judge:
+    def __init__(self, player1, player2, max_attempts, initial_score):
+        self.player1 = player1
+        self.player2 = player2
+        self.max_attempts = max_attempts
+        self.initial_score = initial_score
+        self.question_log = []
+        self.start_time = 0
 
-def get_game_state():
-    r = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
-    state = r.get(GAME_STATE_KEY)
-    if state is None:
-        # Initialize game state if it doesn't exist in Redis
-        state = {
-            'players': DEFAULT_PLAYERS,
-            'scores': [0, 0],
-            'piles': DEFAULT_PILES,
-            'active_player': 0
-        }
-        r.set(GAME_STATE_KEY, str(state))
-    else:
-        state = eval(state.decode('utf-8'))  # Convert bytes to dictionary
-    return state
+    def start_game(self):
+        self.player1.score = self.initial_score
+        self.player2.score = self.initial_score
+        self.question_log = []
+        self.start_time = time.time()
 
-def update_game_state(state):
-    r = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
-    r.set(GAME_STATE_KEY, str(state))
+    def log_question(self, sender, question):
+        current_time = time.time() - self.start_time
+        self.question_log.append(f'[{current_time:.2f}, {sender.name}] {question}')
 
-@app.route('/')
-def home():
-    game_state = get_game_state()
-    return render_template('game.html', game_state=game_state)
+    def log_response(self, sender, response, is_correct):
+        current_time = time.time() - self.start_time
+        self.question_log.append(f'[{current_time:.2f}, {sender.name}] {response}')
+        if is_correct:
+            self.question_log.append(f'[{current_time:.2f}, Judge] OK')
+        else:
+            self.question_log.append(f'[{current_time:.2f}, Judge] NO')
 
-@socketio.on('start_game')
-def handle_start_game(data):
-    game_state = get_game_state()
-    game_state['players'] = [data['player1'], data['player2']]
-    game_state['scores'] = [0, 0]
-    game_state['piles'] = data['piles']
-    game_state['active_player'] = data['active_player']
-    update_game_state(game_state)
-    emit('game_started', {'game_state': game_state}, broadcast=True)
+    def send_question(self, question):
+        self.log_question(self.player1, question)
+        self.log_question(self.player2, question)
 
-@socketio.on('make_move')
-def handle_make_move(data):
-    game_state = get_game_state()
-    move = int(data['move'])
-    stones = int(data['stones'])
-    active_player = game_state['active_player']
-    game_state['piles'][move] -= stones
-    game_state['scores'][active_player] += stones
-    game_state['active_player'] = 1 - active_player  # Toggle active player
-    update_game_state(game_state)
+    def judge_response(self, player, response):
+        if self.player1.score <= 0 or self.player2.score <= 0:
+            return
 
-    # Check if all piles have 0 stones
-    if all(stones == 0 for stones in game_state['piles']):
-        # Declare the winner
-        winner_index = game_state['scores'].index(max(game_state['scores']))
-        game_state['winner'] = game_state['players'][winner_index]
-        emit('winner_declared', {'game_state': game_state}, broadcast=True)
-    else:
-        emit('move_made', {'game_state': game_state}, broadcast=True)
+        self.log_response(player, response, False)
 
+        if self.player1.score == self.player2.score == self.initial_score:
+            self.log_response(self.player1, response, True)
+            self.player1.score += 1
+        elif player == self.player1:
+            if self.player2.score > 0:
+                self.log_response(self.player2, response, True)
+                self.player2.score += 1
+        elif player == self.player2:
+            if self.player1.score > 0:
+                self.log_response(self.player1, response, True)
+                self.player1.score += 1
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+        self.player1.score -= 2
+        self.player2.score -= 2
+
+        if self.player1.score <= 0 or self.player2.score <= 0:
+            self.end_game()
+
+    def end_game(self):
+        self.question_log.append('Game Over')
+
+# Example usage:
+player1 = Player('P1', 5)
+player2 = Player('P2', 5)
+judge = Judge(player1, player2, max_attempts=4, initial_score=5)
+
+judge.start_game()
+judge.send_question('When was the war of 1812?')
+
+# Simulating player responses
+judge.judge_response(player1, '1800')
+judge.judge_response(player2, '1810')
+judge.judge_response(player1, '1812')
+
+# Print the question log
+for log_entry in judge.question_log:
+    print(log_entry)
